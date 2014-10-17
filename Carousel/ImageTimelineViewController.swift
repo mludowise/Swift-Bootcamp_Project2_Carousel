a8a502442b3e1067f40443b685e9b74225148c56
mludowise <mludowise@gmail.com>
2014-10-17 00:13:38 -0700

    Tweaked animation & check for nil for navbar

--- Carousel/ImageTimelineViewController.swift
+++ Carousel/ImageTimelineViewController.swift
@@ -29,8 +29,8 @@ class ImageTimelineViewController: UIViewController {
         // Cache screen size
         screenSize = UIScreen.mainScreen().bounds
         
+        // Size scrollview
         scrollView.contentSize = CGSize(width: screenSize.width, height: feedView.frame.height + banner.frame.height)
-//        scrubberImage.userInteractionEnabled = true
     }
     
     func dismissBanner() {
@@ -61,7 +61,7 @@ class ImageTimelineViewController: UIViewController {
         
         var translation = recognizer.translationInView(scrubberImage)
         var navBar = navigationController?.navigationBar
-        var navBarHeight = navBar!.frame.size.height + navBar!.frame.origin.y
+        var navBarHeight = navBar == nil ? 0 : navBar!.frame.size.height + navBar!.frame.origin.y
         var scrollableHeight = scrollView.contentSize.height - screenSize.height + navBarHeight
         var ratio = translation.x / scrubberImage.frame.width
         var offset = ratio * scrollableHeight
@@ -87,18 +87,23 @@ class ImageTimelineViewController: UIViewController {
     var thumbnailImageView : UIImageView!
 
     @IBAction func onImageTapGesture(tapGestureRecognizer: UITapGestureRecognizer) {
+        // Set the fullScreenImage to be the same size, position, & image as the thumbnail
         thumbnailImageView = tapGestureRecognizer.view as UIImageView
-        fullScreenImageView.image = thumbnailImageView.image
-        
         var newOrigin = view.convertPoint(thumbnailImageView.frame.origin, fromView: thumbnailImageView.superview?)
         fullScreenImageView.frame = CGRect(origin: newOrigin, size: thumbnailImageView.frame.size)
+        fullScreenImageView.image = thumbnailImageView.image
         
-        var imageSize = fullScreenImageView.image!.size
-
+        // Unhide these
         fullScreenImageView.hidden = false
         imageBackgroundView.hidden = false
         
         UIView.animateWithDuration(0.25, animations: { () -> Void in
+            // Fade in background some
+            self.imageBackgroundView.alpha = 0.25
+            
+            var imageSize = self.thumbnailImageView.image!.size
+            
+            // First, resize the image so it's not cropped
             if (imageSize.height > imageSize.width) {
                 self.fullScreenImageView.frame.size.height = imageSize.height / imageSize.width * self.fullScreenImageView.frame.width
                 self.fullScreenImageView.frame.origin.y -= (self.fullScreenImageView.frame.height - self.fullScreenImageView.frame.width)/2
@@ -107,8 +112,12 @@ class ImageTimelineViewController: UIViewController {
                 self.fullScreenImageView.frame.origin.x -= (self.fullScreenImageView.frame.width - self.fullScreenImageView.frame.height)/2
             }
             }, completion: { (b:Bool) -> Void in
-                UIView.animateWithDuration(1, animations: { () -> Void in
+                UIView.animateWithDuration(0.75, animations: { () -> Void in
+                    
+                    // Fade in background all the way
                     self.imageBackgroundView.alpha = 1
+                    
+                    // Resize image to full screen
                     self.fullScreenImageView.contentMode = UIViewContentMode.ScaleAspectFit
                     self.fullScreenImageView.frame.size.width = self.screenSize.width
                     self.fullScreenImageView.frame.size.height = self.screenSize.height
@@ -119,12 +128,16 @@ class ImageTimelineViewController: UIViewController {
     }
     
     @IBAction func onFullScreenImageTapGesture(tapGestureRecognizer: UITapGestureRecognizer) {
-        var imageSize = fullScreenImageView.image!.size
+        // Figure out thumbnail position
         var newOrigin = view.convertPoint(thumbnailImageView.frame.origin, fromView: thumbnailImageView.superview?)
         var thumbnailImageFrame = CGRect(origin: newOrigin, size: thumbnailImageView.frame.size)
         
-        UIView.animateWithDuration(1, animations: { () -> Void in
-            self.imageBackgroundView.alpha = 0
+        UIView.animateWithDuration(0.75, animations: { () -> Void in
+            // Fade out background some
+            self.imageBackgroundView.alpha = 0.75
+            
+            // First, resize image back to the size of the uncropped thumbnail
+            var imageSize = self.fullScreenImageView.image!.size
             if (imageSize.height > imageSize.width) {
                 self.fullScreenImageView.frame.size.width = thumbnailImageFrame.width
                 self.fullScreenImageView.frame.size.height = imageSize.height / imageSize.width * thumbnailImageFrame.width
@@ -138,12 +151,18 @@ class ImageTimelineViewController: UIViewController {
             }
             }, completion: { (b:Bool) -> Void in
                 UIView.animateWithDuration(0.25, animations: { () -> Void in
+                    // Fade out background all the way
+                    self.imageBackgroundView.alpha = 0.25
+                    
+                    // Crop image back to sqaure
                     self.fullScreenImageView.contentMode = UIViewContentMode.ScaleAspectFill
                     self.fullScreenImageView.frame.size.width = thumbnailImageFrame.width
                     self.fullScreenImageView.frame.size.height = thumbnailImageFrame.height
                     self.fullScreenImageView.frame.origin.x = thumbnailImageFrame.origin.x
                     self.fullScreenImageView.frame.origin.y = thumbnailImageFrame.origin.y
                     }, completion: { (b:Bool) -> Void in
+                        
+                        // Hide these
                         self.fullScreenImageView.hidden = true
                         self.imageBackgroundView.hidden = true
                     })
